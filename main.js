32a11dc39210cabdfd7962544880c8696a3ecefc
MaxAguilar712 <a26001977@gmail.com>
2023-08-29 09:47:43 -0400

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,22 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+var i = 0;
+
+while(i < cohortOneStudents.length){
+  console.log(cohortOneStudents[i]);
+  i++;
+}
+
+const myInterests = ['Coding.', 'Guitar.', 'Music.', 'Gaming.', 'Cryptids.'];
+
+let m = 0;
+while(m < 1){
+    console.log('My Absolute Favorite Interest is: ' + myInterests[0])   ;
+    m++; 
+}
+
+while(m < myInterests.length){
+    console.log('One of my interests is: ' + myInterests[m]);
+    m++;
+}
+
